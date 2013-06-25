4443bc18e9603d06cbc71cf49db5e85d971ed8c1
Anton Sizikov <sizikov.anton@gmail.com>
2013-06-25 21:53:35 +0200

    some strings moved to resources

---
+++ src/TimeTable/Resources/LocalizedStrings.cs
@@ -0,0 +1,12 @@
+﻿namespace TimeTable.Resources
+{
+    public sealed class LocalizedStrings
+    {
+        private static readonly Strings LocalizedResources = new Strings();
+
+        public Strings Strings
+        {
+            get { return LocalizedResources; }
+        }
+    }
+}
