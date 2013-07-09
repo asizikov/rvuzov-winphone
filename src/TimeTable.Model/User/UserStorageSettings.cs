8d5c6b45bf0a2d45ae39234b85f26aa8ebde0685
artesdi@gmail.com <artesdi@gmail.com>
2013-07-09 23:01:06 +0400

    add uriMapper and use isolateStorage

---
+++ src/TimeTable.Model/User/UserStorageSettings.cs
@@ -0,0 +1,11 @@
+﻿using JetBrains.Annotations;
+
+namespace TimeTable.Model.User
+{
+    public static class UserStorageSettings
+    {
+        [NotNull]
+        public const string LastPage = "LastPage";
+
+    }
+}
