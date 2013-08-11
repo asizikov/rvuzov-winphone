42f46e90b70648b5503c0d33d7b81fcbfe2ee12b
Anton Sizikov <sizikov.anton@gmail.com>
2013-08-11 20:29:00 +0200

    [#2] app name and main tile updated to russian name

--- src/TimeTable/Properties/AssemblyInfo.cs
+++ src/TimeTable/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Resources;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TimeTable")]
+[assembly: AssemblyTitle("Расписание ВУЗов")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("TimeTable")]
+[assembly: AssemblyProduct("Расписание ВУЗов")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -34,4 +34,4 @@ using System.Resources;
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: NeutralResourcesLanguageAttribute("en-US")]
+[assembly: NeutralResourcesLanguageAttribute("ru")]
