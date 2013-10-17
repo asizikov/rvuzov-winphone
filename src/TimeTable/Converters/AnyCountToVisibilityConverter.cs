9ac4d2f8ba6ca6897940757f9acf0153b212fefa
RussianDjoNIK <zaharcev777@ya.ru>
2013-10-18 00:53:38 +0400

    #36 Добавил конвертер, проверяющий есть ли что-нибудь в IList. И прикрутил его к DataTemplate пар

---
+++ src/TimeTable/Converters/AnyCountToVisibilityConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace TimeTable.Converters
+{
+    public class AnyCountToVisibilityConverter : IValueConverter
+    {
+        #region AnyVisibility
+        private Visibility _anyVisibility = Visibility.Visible;
+        public Visibility AnyVisibility
+        {
+            get { return _anyVisibility; }
+            set { _anyVisibility = value; }
+        }
+        #endregion
+
+        #region EmptyVisibility
+        private Visibility _emptyVisibility = Visibility.Collapsed;
+        public Visibility EmptyVisibility
+        {
+            get { return _emptyVisibility; }
+            set { _emptyVisibility = value; }
+        }
+        #endregion
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var iList = value as IList;
+            if (iList == null)
+            {
+                Debug.WriteLine("В AnyCountToVisibilityConverter передан не IList");
+                iList = new object[]{};
+            }
+
+            return iList.Count > 0 ? AnyVisibility : EmptyVisibility;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException("AnyCountToVisibilityConverter.ConvertBack");
+        }
+    }
+}
