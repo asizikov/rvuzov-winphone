591caf4450ad0a23fcf2fa4ccac1e913a7787b6e
Anton Sizikov <sizikov.anton@gmail.com>
2013-10-20 22:14:57 +0200

    missed page

---
+++ src/TimeTable/Converters/StringContentToBoolConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TimeTable.Converters
+{
+    public class StringContentToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var str = value as string;
+            return (!string.IsNullOrWhiteSpace(str));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
