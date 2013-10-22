414f28fee5be9a03de00263897ffba9b88dd16cb
Anton Sizikov <sizikov.anton@gmail.com>
2013-10-22 22:35:39 +0200

    #7 restore report error page from tombstore state

--- src/TimeTable/View/ReportErrorPage.xaml.cs
+++ src/TimeTable/View/ReportErrorPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Windows.Navigation;
+using TimeTable.Utils;
 using TimeTable.ViewModel;
 using TimeTable.ViewModel.Services;
 
@@ -7,6 +8,9 @@ namespace TimeTable.View
 {
     public partial class ReportErrorPage
     {
+        private string _errotText;
+        private const string ERROR_TEXT_KEY = "LastSearchKey";
+
         public ReportErrorPage()
         {
             InitializeComponent();
@@ -23,6 +27,27 @@ namespace TimeTable.View
             NavigationContext.QueryString.TryGetValue(NavigationParameterName.LessonId, out lessonId);
             
             DataContext = ViewModelLocator.GetReportErrorViewModel(Int32.Parse(id), Int32.Parse(lessonId), Boolean.Parse(isTeacher));
+
+            if (State.Count > 0)
+            {
+                this.RestoreState(ErrorTextTextBox);
+                _errotText = this.RestoreState(ERROR_TEXT_KEY) as string;
+            }
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            base.OnNavigatingFrom(e);
+            State.Clear();
+            if (this.ShouldTombstone(e))
+            {
+                this.SaveState(this.ErrorTextTextBox);
+                if (!string.IsNullOrEmpty(this._errotText))
+                {
+                    this.SaveState(ERROR_TEXT_KEY, _errotText);
+                }
+            }
+            _errotText = string.Empty;
         }
     }
 }
