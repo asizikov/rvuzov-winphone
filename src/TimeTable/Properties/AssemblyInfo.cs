b2f7e9d7b2f44f1677fa42fa378165ec39df357f
Anton Sizikov <sizikov.anton@gmail.com>
2013-10-27 23:08:38 +0100

    screenshots for wp store added

--- src/TimeTable/Properties/AssemblyInfo.cs
+++ src/TimeTable/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Resources;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Расписание ВУЗов")]
+[assembly: AssemblyTitle("Расписание вузов")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Расписание ВУЗов")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyProduct("Расписание вузов")]
+[assembly: AssemblyCopyright("Copyright © Расписание вузов  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
