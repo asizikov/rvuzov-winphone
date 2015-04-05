4f2717fb5e324b4a02fb232b40e753761814c639
Anton <sizikov.anton@gmail.com>
2015-04-06 00:11:09 +0200

    #13 search text box added to header, not found button hidden behind the items list

--- src/TimeTable/View/NewUniversitiesPage.xaml.cs
+++ src/TimeTable/View/NewUniversitiesPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Navigation;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using TimeTable.Mvvm.Navigation;
 using TimeTable.ViewModel;
@@ -6,20 +8,25 @@ using TimeTable.ViewModel.OrganizationalStructure;
 
 namespace TimeTable.View
 {
-	 [DependsOnViewModel(typeof (UniversitiesPageViewModel))]
+    [DependsOnViewModel(typeof (UniversitiesPageViewModel))]
     public partial class NewUniversitiesPage : PhoneApplicationPage
     {
         public NewUniversitiesPage()
         {
             InitializeComponent();
         }
-		
-		protected override void OnNavigatedTo(NavigationEventArgs e)
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
             var navigationContext = NavigationContext.QueryString.RestoreContext<Reason>();
             DataContext = ViewModelLocator.GetUniversitiesViewModel(navigationContext.Body) as ISearchViewModel;
         }
 
+        private void Search_GotFocus(object sender, RoutedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            if (textBox != null) textBox.SelectAll();
+        }
     }
 }
