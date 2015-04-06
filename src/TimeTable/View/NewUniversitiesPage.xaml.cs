c8945e75f499b6d2ea44575b7a35470ef396b51c
Anton <sizikov.anton@gmail.com>
2015-04-06 13:46:30 +0200

    #13 new Universities page is SearchPage now

--- src/TimeTable/View/NewUniversitiesPage.xaml.cs
+++ src/TimeTable/View/NewUniversitiesPage.xaml.cs
@@ -1,15 +1,15 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
-using Microsoft.Phone.Controls;
 using TimeTable.Mvvm.Navigation;
+using TimeTable.Utils;
 using TimeTable.ViewModel;
 using TimeTable.ViewModel.OrganizationalStructure;
 
 namespace TimeTable.View
 {
     [DependsOnViewModel(typeof (UniversitiesPageViewModel))]
-    public partial class NewUniversitiesPage : PhoneApplicationPage
+    public partial class NewUniversitiesPage
     {
         public NewUniversitiesPage()
         {
@@ -20,13 +20,34 @@ namespace TimeTable.View
         {
             base.OnNavigatedTo(e);
             var navigationContext = NavigationContext.QueryString.RestoreContext<Reason>();
-            DataContext = ViewModelLocator.GetUniversitiesViewModel(navigationContext.Body) as ISearchViewModel;
+            ViewModel = ViewModelLocator.GetUniversitiesViewModel(navigationContext.Body) as ISearchViewModel;
+            DataContext = ViewModel;
+
+            if (State.Count > 0)
+            {
+                this.RestoreState(Search);
+                Search.Visibility = (Visibility) this.RestoreState(SearchKey);
+            }
         }
 
         private void Search_GotFocus(object sender, RoutedEventArgs e)
         {
             var textBox = sender as TextBox;
             if (textBox != null) textBox.SelectAll();
+        }
+
+        protected override void SaveState(NavigatingCancelEventArgs e)
+        {
+            if (this.ShouldTombstone(e))
+            {
+                this.SaveState(Search);
+                this.SaveState(SearchKey, Search.Visibility);
+            }
+        }
+
+        protected override void SetFocus()
+        {
+            Search.Focus();
         }
     }
 }
