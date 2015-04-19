dd9e384f360bda4aa3305cbdc19ac125356d5f09
Anton <sizikov.anton@gmail.com>
2015-04-19 13:24:12 +0200

    #21 Small header extracted to UserControl

---
+++ src/TimeTable/Controls/TimeTableSmallHeader.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace TimeTable.Controls
+{
+    public partial class TimeTableSmallHeader : UserControl
+    {
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof (string), typeof (TimeTableSmallHeader), null);
+
+        public string Text
+        {
+            get { return GetValue(TextProperty) as string; }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public TimeTableSmallHeader()
+        {
+            InitializeComponent();
+        }
+    }
+}
